ca499d530aa4326ddb09f4b7ced4aa4075923ef9
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-10-23 17:46:58 +0700

    add csv_read()

--- util/include/util/util.h
+++ util/include/util/util.h
@@ -58,6 +58,62 @@ void csv_write(const MatType& m, const std::string& csv_path) {
 /*!
  * @brief
  */
+template<typename MatType>
+MatType CSVIO::read(const std::string& csv_path) {
+  // Adopted from: https://gist.github.com/stephenjbarr/2266900
+  using namespace std;
+  using namespace boost;
+  
+  ifstream csv(csv_path);
+  BOOST_ASSERT_MSG(csv.is_open(), string("ERROR: Cannot find: " + csv_path).c_str());
+  
+  typedef tokenizer< escaped_list_separator<char> > Tokenizer;
+  
+  vector< string > vec;
+  string line;
+  vector< vector< string > > matrows;
+  
+  while (getline(csv,line)) {
+    Tokenizer tok(line);
+    vec.assign(tok.begin(),tok.end());
+    matrows.push_back(vec);
+  }
+  csv.close();
+  
+  // FIGURE OUT HOW MANY OF THE ROWS HAVE THE RIGHT NUMBER
+  // OF COLUMNS
+  int Nrows = matrows.size();
+  int Ncols = matrows[0].size();
+  int Ngoodrows = 0;
+  for(int i = 0; i < Nrows; i++) {
+    if(matrows[i].size() == Ncols) {
+      Ngoodrows++;
+    }
+  }
+  
+  // TRANSFORM THE VECTOR OF ROWS INTO AN EIGEN DOUBLE MATRIX
+  MatType xmat = MatType(Ngoodrows, Ncols);
+  //cout << "INPUT MATRIX: " << Nrows << "x" << Ncols << endl;
+  int rc = 0;
+  for(int i = 0; i < Nrows; i++) {
+    int rowsize = matrows[i].size();
+    if(rowsize != Ncols) {
+      cout << "WARNING: Row " << i << " has bad column count" << endl;
+      continue;
+    } 
+    
+    for(int j = 0; j < Ncols; j++) {
+      xmat(rc,j) = int( round(strtod(matrows[i][j].c_str(), NULL)) );
+      xmat(rc,j) = lexical_cast<double>(matrows[i][j]);
+    }
+    rc++;
+  }
+  
+  return xmat;
+}
+/*!
+ * @brief
+ */
 size_t n_pairwise(const size_t& n_row, const size_t& n_col, const std::string& type);
 
 /*!
