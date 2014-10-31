ffbddd931b4d4df2cca7270a9237d92fc7852972
jogie chandra <chandra.jogie@gmail.com>
2014-10-31 16:56:26 +0700

    produce experiment code  with overlap superpixel

---
+++ overlap-exp/src-py/majority-vote.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+
+# def do_majority_vote_image():
+
+def main():
+  # param 1 : img_test_list, param 2 : param_superpixel, param 3 : dir_ann, param 4 : dir_final_res
+  if len(sys.argv) != 5:
+    assert False, "INSUFFICIENT ARGUMENTS!"
+
+if __name__ == "__main__":
+  main();
