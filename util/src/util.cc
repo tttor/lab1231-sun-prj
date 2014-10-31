80d40fa96a2a434ab46eee95c41512bf7fd1f369
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-10-31 18:20:30 +0700

    add err msgs

--- util/src/util.cc
+++ util/src/util.cc
@@ -49,6 +49,9 @@ std::vector<std::string> read_list(const std::string& list_path) {
     }
     test_img_list_file.close();
   }
+  else {
+    cerr << "test_img_list_file.is_open(): FALSE\n";
+  }
 
   return test_img_filenames;
 }
