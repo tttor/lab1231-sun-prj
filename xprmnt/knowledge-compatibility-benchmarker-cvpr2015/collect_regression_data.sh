a4e26cedbf3a25b36b90dd6a78608f42b908be2b
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-11-12 16:56:09 +0700

    collect regression data for Kc and Ks

--- xprmnt/knowledge-compatibility-benchmarker-cvpr2015/collect_regression_data.sh
+++ xprmnt/knowledge-compatibility-benchmarker-cvpr2015/collect_regression_data.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 #
-ann_list_filename=ann_img_dummy.list
-knowledge_dir=/home/tor/sun4/xprmnt/knowledge-compatibility-benchmarker-cvpr2015/knowledge
-ann_list_filepath=/home/tor/sun4/xprmnt/knowledge-compatibility-benchmarker-cvpr2015/meta/split_voc2010_tor/$ann_list_filename
-ann_dir=/home/tor/sun4/xprmnt/knowledge-compatibility-benchmarker-cvpr2015/annotation
-out_dir=/home/tor/sun4/xprmnt/knowledge-compatibility-benchmarker-cvpr2015/regression-data
+root_dir=/home/tor/sun4/xprmnt/knowledge-compatibility-benchmarker-cvpr2015
+
+ann_list_filename=ann_img.list
+ann_list_filepath=$root_dir/meta/split-voc2010-tor/$ann_list_filename
+
+knowledge_dir=$root_dir/knowledge
+ann_dir=$root_dir/annotation/annotation-philippunary
+
+out_dir=$root_dir/regression-data
+regression_data_filename=voc2010_philippunary.csv
+regression_data_filepath=$out_dir/$regression_data_filename
+
+rm $regression_data_filepath
 
 #
 exe=collect_regression_data.py
-
-python $exe $ann_list_filepath $knowledge_dir $ann_dir $out_dir
+python $exe $ann_list_filepath $knowledge_dir $ann_dir $regression_data_filepath
