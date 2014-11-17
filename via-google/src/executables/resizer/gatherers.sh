90fe3b2f5847855cb62e0cfc4145cff0d8ff1d60
rizkivmaster <rizkivmaster@gmail.com>
2014-11-17 23:35:14 +0700

    add resizer

--- via-google/src/executables/resizer/gatherers.sh
+++ via-google/src/executables/resizer/gatherers.sh
@@ -2,6 +2,8 @@ source ../config
 target=$googleimgfolder
 echo "Target cropped image folder:"
 echo "$target"
+echo "target classlist file:"
+echo "$classlistfile"
 echo "######################"
 
 
@@ -19,15 +21,16 @@ IFS=$'\n' read -d '' -r -a imgfolds < imgfolddir
 #`rm allclasssmrf`
 
 c=0
-#max=`wc -l < $1`
-max=1
+max=`wc -l < $classlistfile`
+#max=1
 echo $max
-while [ $c -le $max ]
+while [ $c -lt $max ]
 do
 	#`rm ${names[$c]}smrf`
 	imgnames=`ls -d ${imgfolds[$c]}/*`
 	for imgname in $imgnames		
 	do
+		echo "resizing $imgname"
 		mogrify -resize '500x500>' $imgname
 	done
 	(( c++ ))
