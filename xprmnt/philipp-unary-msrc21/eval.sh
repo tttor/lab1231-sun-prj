407a983a2b05ba6b6499e0f74b3bba6b765291bf
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-11-20 14:11:03 +0700

    xprmt with philipp-unary only

---
+++ xprmnt/philipp-unary-msrc21/eval.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+curr_dir="$(dirname "$0")"
+. "$curr_dir/set_dataset_param.sh"
+
+#
+root_dir=/home/tor/sun4/xprmnt/philipp-unary-msrc21
+
+pred_dir=$root_dir/annotation/test
+target_listpath=$root_dir/meta/split-msrc-tor/Test_245.txt
+
+#
+exe=../../util/src-py/evaluator_tor.py 
+python $exe $target_listpath $pred_dir $gt_csv_dir
