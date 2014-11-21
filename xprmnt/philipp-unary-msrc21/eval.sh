4c7349aa5426fc6319f46abfe62b99bf0db70fde
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-11-21 13:11:06 +0700

    run testing with 93 GT

--- xprmnt/philipp-unary-msrc21/eval.sh
+++ xprmnt/philipp-unary-msrc21/eval.sh
@@ -6,7 +6,7 @@ curr_dir="$(dirname "$0")"
 root_dir=/home/tor/sun4/xprmnt/philipp-unary-msrc21
 
 pred_dir=$root_dir/annotation/test
-target_listpath=$root_dir/meta/split-msrc-tor/Test_245.txt
+target_listpath=$root_dir/meta/split-msrc-tor/Test_93.txt
 
 #
 exe=../../util/src-py/evaluator_tor.py 
--- xprmnt/philipp-unary-msrc21/set_dataset_param.sh
+++ xprmnt/philipp-unary-msrc21/set_dataset_param.sh
@@ -5,5 +5,5 @@ dataset_name=msrc
 n_label=21 #
 ori_img_dir=/home/tor/sun3/dataset/msrc/unmix/Images
 gt_img_dir=/home/tor/sun3/dataset/msrc/unmix/GroundTruth
-gt_csv_dir=/home/tor/sun3/dataset/msrc/unmix/GroundTruth-csv-245
+gt_csv_dir=/home/tor/sun3/dataset/msrc/unmix/GroundTruth-csv
 unary_philipp_dir=/home/tor/sun3/dataset/msrc/unary_philipp/msrc_compressed
