21ce15a0def1f94d30f945e79c7c7894645141b0
rizkivmaster <rizkivmaster@gmail.com>
2014-11-23 04:39:04 +0700

    add simple logging system

--- via-google/transform.sh
+++ via-google/transform.sh
@@ -14,6 +14,13 @@ source config
 `cp config $current_exp`
 `cp $settings_path $current_exp`
 
+logpath="$current_exp/log$RANDOM"
+
+
+echo "START TRANSFORM" > $logpath
+date +"%m-%d-%y %T" >> $logpath
+
+
 #download google images
 # cd google-retriever
 # ./execute.sh
@@ -27,30 +34,57 @@ source config
 # ./execute.sh
 # cd ..
 # #train initial parameters START HERE
+
+echo "START GENERATING INITIAL MODEL" >> $logpath
+date +"%m-%d-%y %T" >> $logpath
+
 cd param-generator
 ./execute.sh
 cd ..
 # #generate google unaries
+
+echo "START GENERATING GOOGLE UNARY" >> $logpath
+date +"%m-%d-%y %T" >> $logpath
+
 cd google-unary-generator
 ./execute.sh
 cd ..
 # # #commit F/G segmentation
+
+echo "START F/G SEGMENTATION" >> $logpath
+date +"%m-%d-%y %T" >> $logpath
+
 cd fgannotator
 ./execute.sh
 cd ..
 # # #train voc with google/ COMBINATION
+
+echo "START IMPROVING MODEL" >> $logpath
+date +"%m-%d-%y %T" >> $logpath
+
 cd param-enhancer
 ./execute.sh
 cd ..
 # # #generate test unary DTE
+echo "START TEST UNARY GENERATION" >> $logpath
+date +"%m-%d-%y %T" >> $logpath
+
 cd test-unary-generator
 ./execute.sh
 cd ..
 # # #perform multiclass segmentation
+echo "START TESTSET ANNOTATION" >> $logpath
+date +"%m-%d-%y %T" >> $logpath
+
 cd multiannotation
 ./execute.sh
 cd ..
+
 # #performance evaluation
+
+echo "START EVALUATION" >> $logpath
+date +"%m-%d-%y %T" >> $logpath
+
 cd evaluation
 ./execute.sh
 cd ..
