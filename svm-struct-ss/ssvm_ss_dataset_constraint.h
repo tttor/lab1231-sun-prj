6a32d14b1d6d33ddcfa1ce462ea65f4e56598367
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-12-02 16:06:44 +0700

    init ssvm work

---
+++ svm-struct-ss/ssvm_ss_dataset_constraint.h
@@ -0,0 +1,14 @@
+#ifndef SSVM_SS_DATASET_CONSTRAINT_H
+#define SSVM_SS_DATASET_CONSTRAINT_H
+
+#include <string>
+
+namespace ssvm_ss {
+namespace dataset {
+
+const size_t n_example = 100;
+
+}// namespace dataset
+}// namespace ssvm_ss
+
+#endif
---
+++ svm-struct-ss/ssvm_ss_image_constraint.h
@@ -0,0 +1,14 @@
+#ifndef SSVM_SS_IMAGE_CONSTRAINT_H
+#define SSVM_SS_IMAGE_CONSTRAINT_H
+
+namespace ssvm_ss {
+namespace image_constraint {
+
+const size_t height = 213;
+const size_t width = 320;
+const size_t n_pixel = width*height;
+
+}// namespace dataset
+}// namespace ssvm_ss
+
+#endif
