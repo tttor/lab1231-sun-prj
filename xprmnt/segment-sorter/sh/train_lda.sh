3d627ec054c77617e43115c9354960edccf4b748
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-12-07 12:57:06 +0700

    cp corpus metadata

--- xprmnt/segment-sorter/sh/train_lda.sh
+++ xprmnt/segment-sorter/sh/train_lda.sh
@@ -4,12 +4,14 @@ root_dir=/home/tor/xprmnt/segment-sorter
 run_id="$(date +'%Y%m%d.%H%M%S')"
 
 #
-tr_data_filename=corpus.20141207.085033.data
-tr_data_filepath=$root_dir/corpus/$tr_data_filename
+corpus_name=corpus.20141207.085033
+tr_data_filepath=$root_dir/corpus/$corpus_name.data
+tr_metadata_filepath=$root_dir/corpus/$corpus_name.meta
 
 model_dir=$root_dir/lda-model/training.$run_id
 mkdir -p $model_dir
 cp $tr_data_filepath $model_dir #as the LDAexe put the resulted model in the same dir as the inputted training_data
+cp $tr_metadata_filepath $model_dir
 
 #
 dfile=$model_dir/$tr_data_filename
