0b68d5ac8afc0da93fa638c33b5b85f08ed55e57
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-12-10 00:13:15 +0700

    write clustering input

--- xprmnt/segment-sorter/sh/cluster_words.sh
+++ xprmnt/segment-sorter/sh/cluster_words.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 #
-root_dir=/home/tor/xprmnt/segment-sorter
+root_dir=/home/tor/sun4/xprmnt/segment-sorter
 timestamp="$(date +'%Y%m%d.%H%M%S')"
 dataset=msrc21
 region_desc=sift
 
 #
-root_dir=/home/tor/xprmnt/segment-sorter
-
 list_filepath=$root_dir/meta/$dataset.$region_desc.list
 region_dir=$root_dir/region/$dataset/$region_desc
 
@@ -18,6 +16,7 @@ mkdir -p $clustered_region_dir
 kmeans_model_dir=$root_dir/kmeans-model/$dataset
 mkdir -p $kmeans_model_dir
 kmeans_model_filepath=$kmeans_model_dir/kmeans.$timestamp.model
+kmeans_inputdata_filepath=$kmeans_model_dir/kmeans.$timestamp.inputdata
 
 #
 n_word_cluster=2000
@@ -25,10 +24,10 @@ max_iter=300
 n_init=10
 init=k-means++
 
-export PYTHONPATH=$PYTHONPATH:/home/tor/ws/lab1231-sun-prj/segment-sorter/common
+export PYTHONPATH=$PYTHONPATH:/home/tor/sun/ws/lab1231-sun-prj/segment-sorter/common
 exe=../../../segment-sorter/topic-discovery/cluster_words.py
 
 python  $exe \
 		$list_filepath $region_dir \
-		$clustered_region_dir $kmeans_model_filepath \
+		$clustered_region_dir $kmeans_model_filepath $kmeans_inputdata_filepath\
 		$n_word_cluster $max_iter $n_init $init
