9d02c3a0e144e6fce2878ffe5bde34b366fe5f25
tttor <vektor.dewanto@gmail.com>
2014-12-09 22:43:48 +0700

    add dataset vars

--- xprmnt/segment-sorter/sh/cluster_words.sh
+++ xprmnt/segment-sorter/sh/cluster_words.sh
@@ -2,18 +2,20 @@
 #
 root_dir=/home/tor/xprmnt/segment-sorter
 timestamp="$(date +'%Y%m%d.%H%M%S')"
+dataset=msrc21
+region_desc=sift
 
 #
 root_dir=/home/tor/xprmnt/segment-sorter
 
-list_filepath=$root_dir/meta/msrc-21.sift.list
-region_dir=$root_dir/region/msrc-21/sift
+list_filepath=$root_dir/meta/$dataset.$region_desc.list
+region_dir=$root_dir/region/$dataset/$region_desc
 
-clustered_region_dir=$root_dir/region-clustered/msrc-21/sift
+clustered_region_dir=$root_dir/region-clustered/$dataset/$region_desc
 rm -rf $clustered_region_dir
 mkdir -p $clustered_region_dir
 
-kmeans_model_dir=$root_dir/kmeans-model
+kmeans_model_dir=$root_dir/kmeans-model/$dataset
 mkdir -p $kmeans_model_dir
 kmeans_model_filepath=$kmeans_model_dir/kmeans.$timestamp.model
 
