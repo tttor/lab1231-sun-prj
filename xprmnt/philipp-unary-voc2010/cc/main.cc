8369bdc10395804382508e45edfea2d2e1beeba2
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-12-18 18:54:28 +0700

    receive filename w/o extension

--- xprmnt/philipp-unary-voc2010/cc/main.cc
+++ xprmnt/philipp-unary-voc2010/cc/main.cc
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]) {
     Eigen::MatrixXi ann;
     ann = annotate(img_filename, data_param);
 
-    const string ann_filepath = string(data_param["result_dir"]+img_filename.substr(0,img_filename.size()-4)+".csv");
+    const string ann_filepath = string(data_param["result_dir"]+img_filename+".csv");
     sun::util::csv_write<Eigen::MatrixXi>(ann, ann_filepath);
     // const string ann_img_filepath = string(data_param["result_dir"]+img_filename.substr(0,img_filename.size()-4)+".bmp");
     // cv::imwrite(ann_img_filepath, sun::util::ann2img(ann, data_param["dataset_name"]));
@@ -45,7 +45,7 @@ Eigen::MatrixXi annotate(const std::string& img_filename, sun::util::DataParam d
   using namespace std;
   // std::cout << "annotate(): BEGIN\n";
 
-  const string img_path = string(data_param["ori_img_dir"] + img_filename);
+  const string img_path = string(data_param["ori_img_dir"] + img_filename + ".jpg");
   // cout << "img_path= " << img_path << endl;
 
   cv::Mat img = cv::imread(img_path, CV_LOAD_IMAGE_COLOR);
@@ -70,7 +70,7 @@ Eigen::MatrixXi annotate(const std::string& img_filename, sun::util::DataParam d
 void set_1st_order(const std::string& img_filename, const size_t& n_label, const std::string& unary_prob_img_dir, GraphicalModel* gm) {
   using namespace std;
 
-  const string unary_prob_img_path = string( unary_prob_img_dir+img_filename.substr(0,img_filename.size()-4)+".c_unary" );// -4 for .bmp
+  const string unary_prob_img_path = string( unary_prob_img_dir+img_filename+".c_unary" );
   // cout << "unary_prob_img_path= " << unary_prob_img_path << endl;
 
   ProbImage unary_prob_img;
@@ -100,8 +100,8 @@ void set_1st_order(const std::string& img_filename, const size_t& n_label, const
 
 void infer(const std::string& method, const GraphicalModel& gm, const size_t& n_var, Eigen::MatrixXi* ann) {
   using namespace std;
-  cout << "infer(): BEGIN\n";
-  cout << "method= " << method << endl;
+  // cout << "infer(): BEGIN\n";
+  // cout << "method= " << method << endl;
 
   vector<size_t> ann_vec(n_var);
   
@@ -120,7 +120,7 @@ void infer(const std::string& method, const GraphicalModel& gm, const size_t& n_
 
     MinAlphaExpansion inf_engine(gm);
 
-    cout << "Inferring ..." << endl;
+    cout << "Inferring with " << method << "..." << endl;
     inf_engine.infer();
     inf_engine.arg(ann_vec);
   }
