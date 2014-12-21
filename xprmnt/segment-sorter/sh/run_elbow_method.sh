c8be847a1397d989177f7291c3aa3d8ee21faf3d
tttor <vektor.dewanto@gmail.com>
2014-12-21 16:49:30 +0700

    fix inputfile

--- xprmnt/segment-sorter/sh/run_elbow_method.sh
+++ xprmnt/segment-sorter/sh/run_elbow_method.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #
-run_id=test01
+run_id=4678.5000
 root_dir=/home/tor/sun4/xprmnt/segment-sorter/kmeans-model
 dataset=msrc21
 
 # Determine a list of n_cluster
 # >>> K = ''.join([str(int(i))+' ' for i in list(numpy.linspace(50,200,10))])
-K='1 12 23 34 45 56 67 78 89 100 '
+#K='500 821 1142 1464 1785 2107 2428 2750 3071 3392 3714 4035 4357 4678 5000 '
+#K='821 1142 '
+#K='2107 '
+K='4678 5000 '
 
 #
-py_ws_dir=/home/tor/robotics/prj/011/ws/lab1231-sun-prj/segment-sorter/topic-discovery
+py_ws_dir=/home/tor/sun/ws/lab1231-sun-prj/segment-sorter/topic-discovery
 input_dir=/home/tor/sun4/xprmnt/segment-sorter/kmeans-model/$dataset
 
-input_filename=kmeans.input.csv
+input_filename=kmeans.20141211.172830.input
 input_filepath=$input_dir/$input_filename
 
 out_dir=$root_dir/$dataset/elbow-method.$run_id/meandistortion
