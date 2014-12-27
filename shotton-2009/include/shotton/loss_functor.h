08df7b5c904b8784e3c405b28011b7223c001b02
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-12-27 22:03:57 +0700

    fix notes

--- shotton-2009/include/shotton/loss_functor.h
+++ shotton-2009/include/shotton/loss_functor.h
@@ -5,7 +5,7 @@ namespace lab1231_sun_prj {
 namespace shotton {
 
 /*
-The class ZeroOneLossFunctor is used as a functor to be attached to the OPENGM'sfactor graph
+The class ZeroOneLossFunctor is used as a functor to be attached to the OPENGM's factor graph
 whenever a loss augmented inference is required.
 
 In loss augmented inference, we aim to maximize the loss.
@@ -13,12 +13,13 @@ It is equivalent to minimize the negative loss.
 Thus:
 y_star = argmin E(.) + (-loss)
 
-Notice that due to OPENGM implementation limitation (cannot accomodate the highest term),
-the operator() always returns the minimum loss, i.e. -1.0 for the last node and whenever active=true.
+Notice that due to OPENGM implementation limitation (cannot accomodate a factor having all pixels (the highest term)),
+the operator() always returns the minimum loss, i.e. -1.0 for the last node whenever active=true.
 This is a kind of approximations, but we will get it right almost all the time since loss=0 occurs only at once,
 iff y_true[i] == y[i] for all element i.
-That said, we will get it wrong at one occasion with low probability: 
-when y*==y_true and (E(y*) + 0.0) < (E(y*) + (-1.0))
+
+Due to always returning (loss = -1.0), we will get it wrong,
+iff $ E(y_true) + (-1.0) > E(y) + (-1.0), \forall y \in Y\y_true $
 */
 template<typename T>
 class ZeroOneLossFunctor {
@@ -36,7 +37,6 @@ class ZeroOneLossFunctor {
  private:
    bool last_node_;
    bool active_;
-   
 };
 
 }
