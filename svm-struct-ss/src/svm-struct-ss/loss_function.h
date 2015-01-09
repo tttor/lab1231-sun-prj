bc87b2adc84a3b1f801cfd9a3095d5992881b359
Vektor Dewanto <vektor.dewanto@gmail.com>
2015-01-09 15:28:30 +0700

    add hamming loss

--- svm-struct-ss/src/svm-struct-ss/loss_function.h
+++ svm-struct-ss/src/svm-struct-ss/loss_function.h
@@ -6,11 +6,7 @@
 namespace svm_struct_ss {
 namespace loss_function {
 
-double zero_one_loss(const LABEL& y, const LABEL& ybar, 
-                     const bool& as_sum_of_label_element_losses=false,
-                     const bool& normalized=false) {
-  debug_in_msg("zero_one_loss");
-  debug_var("as_sum_of_label_element_losses",as_sum_of_label_element_losses);
+double zero_one_loss(const LABEL& y, const LABEL& ybar) {
   assert(y.size==ybar.size && "y.size!=ybar.size");
 
   double loss = 0.0; // optimistic init
@@ -19,20 +15,26 @@ double zero_one_loss(const LABEL& y, const LABEL& ybar,
       continue;
 
     if (y.flatten_label[i] != ybar.flatten_label[i]) {
-      if (as_sum_of_label_element_losses) {
-        loss = loss + 1.0;
-      }
-      else {
-        loss = 1.0;
-        break;  
-      }
-    }
+      loss = 1.0;
+      break;  
+    } 
   }
+  return loss;
+}
 
-  if (normalized) 
-    loss = loss / y.size;
+double hamming_loss(const LABEL& y, const LABEL& ybar) {
+  assert(y.size==ybar.size && "y.size!=ybar.size");
 
-  debug_out_msg("zero_one_loss");
+  double loss = 0.0; // optimistic init
+  for (size_t i=0; i<y.size; ++i) {
+    if (y.flatten_label[i]==svm_struct_ss::data_param::void_label) 
+      continue;
+
+    if (y.flatten_label[i] != ybar.flatten_label[i])
+      loss = loss + 1.0;
+  }
+  loss = loss / y.size;
+
   return loss;
 }
 
