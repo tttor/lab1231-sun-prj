86a6f2f4565bf735ac29c3fbd5bd1805d7297064
vektor dewanto <vektor.dewanto@gmail.com>
2015-03-08 10:28:33 +0700

    use all 1928 imgs

--- xprmnt/knowledge-construction/make_cooccurrence_knowledge.sh
+++ xprmnt/knowledge-construction/make_cooccurrence_knowledge.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-root_dir=/home/tor/xprmnt/knowledge-construction/cooccurrence
+root_dir=/home/tor/xprmnt/knowledge-construction/cooccurrence-knowledge
 timestamp="$(date +'%Y%m%d.%H%M%S')"
 
-ann_list_filepath=/home/tor/dataset/pascal/VOC2010/meta/split_voc2010_philipp/Train.txt.ori
+ann_list_filepath=/home/tor/dataset/pascal/VOC2010/meta/split_voc2010_philipp/All_1928.txt
 csv_ann_dir=/home/tor/dataset/pascal/VOC2010/VOCdevkit/VOC2010/SegmentationClass-csv
 knowledge_dir=$root_dir
 
--- xprmnt/knowledge-construction/make_scene_property_knowledge.sh
+++ xprmnt/knowledge-construction/make_scene_property_knowledge.sh
@@ -3,7 +3,7 @@
 root_dir=/home/tor/xprmnt/knowledge-construction/scene-property-knowledge
 timestamp="$(date +'%Y%m%d.%H%M%S')"
 
-ann_list_filepath=/home/tor/dataset/pascal/VOC2010/meta/split_voc2010_philipp/Train.txt.ori
+ann_list_filepath=/home/tor/dataset/pascal/VOC2010/meta/split_voc2010_philipp/All_1928.txt
 scene_prop_ann_dir=/home/tor/dataset/pascal/VOC2012/VOC2012/ScenePropertyAnnotations/cikupastar-20141111/ScenePropertyAnnotations-2
 knowledge_dir=$root_dir
 
