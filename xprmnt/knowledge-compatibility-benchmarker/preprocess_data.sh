6fbd620695ad221e323d522544e0853017c11b16
vektor dewanto <vektor.dewanto@gmail.com>
2015-03-15 17:49:10 +0700

    rm vars

--- xprmnt/knowledge-compatibility-benchmarker/preprocess_data.sh
+++ xprmnt/knowledge-compatibility-benchmarker/preprocess_data.sh
@@ -7,10 +7,6 @@ dataset_name=voc2010
 rawdata_timestamp=20150314.224909
 
 regression_data_dirpath=$root_dir/$annotator_id-$dataset_name-$rawdata_timestamp/input
-fea_name=cooccurrence_fea
-# fea_name=sceneprop_fea
-# fea_name=relloc_fea
-
 declare -a fea_names=("cooccurrence_fea" "sceneprop_fea" "relloc_fea")
 
 for fea_name in "${fea_names[@]}"
