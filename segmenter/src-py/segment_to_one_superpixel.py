63dd08774f781bd47406a318277b262bbbed2a4f
vektor dewanto <vektor.dewanto@gmail.com>
2015-03-28 13:20:14 +0700

    generate one-sup (trivial) segmentation

---
+++ segmenter/src-py/segment_to_one_superpixel.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import os
+import sys
+import numpy as np 
+from skimage.util import img_as_float
+from skimage import io
+
+def main(argv):
+    img_dir = argv[1]
+    img_list_filepath = argv[2]
+    img_extension = argv[3]
+    out_dir = argv[4]
+
+    # read the list
+    with open(img_list_filepath) as f:
+        img_ids = f.readlines()
+    img_ids = [x.strip('\n') for x in img_ids]
+
+    # 
+    for i, img_id in enumerate(img_ids):
+        print 'Segmenting ith=', i, 'of', len(img_ids)
+
+        img_filepath = img_dir+'/'+img_id+img_extension
+        img = img_as_float(io.imread(img_filepath))
+
+        #
+        shape = (img.shape[0], img.shape[1])
+        segmentation = np.zeros(shape)
+
+        segmentation_dir = out_dir+'/'+img_id
+        if not os.path.exists(segmentation_dir):
+            os.makedirs(segmentation_dir)
+
+        segmentation_filepath = segmentation_dir+'/'+img_id+'-onesuperpixel'+'.sup'
+        np.savetxt(segmentation_filepath, segmentation, delimiter=",")
+
+if __name__ == '__main__':
+    main(sys.argv)
