d6e71bca5a14b23333df89ebf96371f8d057ffdb
vektor dewanto <vektor.dewanto@gmail.com>
2015-03-31 19:04:28 +0700

    add c_str()

--- util/include/util/util.h
+++ util/include/util/util.h
@@ -61,7 +61,7 @@ MatType csv_read(const std::string& csv_path) {
   using namespace std;
   using namespace boost;
   
-  ifstream csv(csv_path);
+  ifstream csv(csv_path.c_str());
   BOOST_ASSERT_MSG(csv.is_open(), string("ERROR: Cannot find: " + csv_path).c_str());
   
   typedef tokenizer< escaped_list_separator<char> > Tokenizer;
